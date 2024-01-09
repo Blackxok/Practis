24f0ae78b12c5dfe1c5a3da5b5215b3b32afa07a
Blackxok <nasridoni1@gmail.com>
2024-01-09 23:29:53 +0500

    cng vbr

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,17 +1,19 @@
-import { IconButton, Paper } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import { Paper } from "@mui/material";
+import { search_Icon } from "../../constant";
 import React from "react";
 import "./Search.css";
 
 function SearchB() {
    return (
       <Paper
+         className="inputPaper"
          component={"form"}
          sx={{ border: "1px solid #568956" }}
-         className="inputPaper"
       >
-         <input type="text" placeholder="search" />
-         
+         <div className="searchBar">
+            <input type="text" placeholder="search" />
+            <img src={search_Icon} alt="searchIcon" />
+         </div>
       </Paper>
    );
 }
--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -1,2 +1,3 @@
 export { default as logo4 } from "../assets/logo.png";
+export { default as search_Icon } from "../assets/searchmagnifierinterfacesymbol1_79893.svg";
 export { default as Search } from "../components/search/Search";
