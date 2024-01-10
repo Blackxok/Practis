2722d4fa4bf6bea9f41fdce298b6626dcea81c7f
Blackxok <nasridoni1@gmail.com>
2024-01-10 12:43:18 +0500

    pld

---
+++ src/components/app/App.jsx
@@ -0,0 +1,22 @@
+// import { Routes, Route } from "react-router-dom";
+// import { Box } from "@mui/material";
+// import { Channel, Main, Search, Navbar, VideoD } from "../index";
+
+function app() {
+   return (
+      <div>
+         {/* <Box>
+            <Navbar />
+            <Routes>
+                <Route path="/" element= {<Main/>}/>
+                <Route path="/" element= {<Channel/>}/>
+                <Route path="/" element= {<Search/>}/>
+                <Route path="/" element= {<VideoD/>}/>
+            </Routes>
+         </Box> */}
+         <h1>Im Working</h1>
+      </div>
+   );
+}
+
+export default app;
