7a7931270b2d3c27aa7926d3c4b6f2e527b86abd
Blackxok <nasridoni1@gmail.com>
2024-01-19 17:36:54 +0500

    cng

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import Navbar from "./Navbar";
 import Category from "./Category";
 import Home from "./Home";
-<<<<<<< HEAD
-=======
 import { CatContext } from "./context/context";
 import { useState } from "react";
->>>>>>> a85e31c2e70ca230b407004ffcbbacab6d14d07a
 
 function App() {
    const [giveD, setGiveD] = useState("");
@@ -13,20 +10,13 @@ function App() {
       setGiveD(e);
    };
 
-
    return (
       <div>
-<<<<<<< HEAD
-         <Navbar />
-         <Category />
-         <Home />
-=======
          <CatContext.Provider value={giveD}>
             <Navbar />
             <Category giveData={giveData} />
             <Home />
          </CatContext.Provider>
->>>>>>> a85e31c2e70ca230b407004ffcbbacab6d14d07a
       </div>
    );
 }
