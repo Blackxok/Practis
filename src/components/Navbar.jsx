34291e2ad7840fa9211cebb69ece55da4c029c45
Blackxok <nasridoni1@gmail.com>
2024-02-15 12:08:35 +0500

    cng sch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Paper, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Search } from "@mui/icons-material";
 import { Log } from "../constants";
 
 function Navbar() {
+   const [value, setValue] = useState("");
+   // 
+   const change = (e) => {
+      setValue(e.target.value);
+   };
+
+
    return (
       <Stack className="container">
          <Link to={"/"}>
@@ -12,12 +19,15 @@ function Navbar() {
          </Link>
          <Box className="poiskBar">
             <Paper className="inputPaper">
-               <input type="text" className="inputSame" placeholder="search" />
+               <input
+                  type="text"
+                  className="inputSame"
+                  placeholder="search"
+                  onChange={change}
+                  value={value}
+               />
             </Paper>
-            <Search
-               className="searchIcon"
-               sx={{ fontSize: `calc(var(--size) * 2)` }}
-            />
+            <Search className="searchIcon" />
          </Box>
       </Stack>
    );
